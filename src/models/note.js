af0e680ae1f84ede2292eb3e4f30df049e06b9e6
87233 <8723323@gmail.com>
2026-02-02 00:05:16 +0200

    f

--- src/models/note.js
+++ src/models/note.js
@@ -18,9 +18,15 @@ const noteSchema = new Schema(
       default: 'Todo',
       enum: TAGS,
     },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
   },
   {
     timestamps: true,
+    versionKey: false,
   },
 );
 
--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -17,7 +19,16 @@ app.use(
     limit: '100kb',
   }),
 );
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
+
+app.use(cookieParser());
+
+app.use(authRoutes);
 
 app.use(notesRoutes);
 
